74b7bc7f248e88fd4252875fc12fab2af5d81c43
Emanuel Duss <emanuel.duss@gmail.com>
2021-02-08 19:05:42 +0100

    Added raw mode checkbox, moved reset message button to top

--- src/main/java/gui/SamlPanelAction.java
+++ src/main/java/gui/SamlPanelAction.java
@@ -6,14 +6,7 @@ import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 import model.BurpCertificate;
 import application.SamlTabController;
@@ -39,6 +32,7 @@ public class SamlPanelAction extends JPanel {
 	private JButton btnSendCertificate;
 	private JButton btnSignatureAdd;
 	private JTextField txtSearch;
+	private JCheckBox chkRawMode;
 
 	public SamlPanelAction() {
 		initialize();
@@ -51,27 +45,63 @@ public class SamlPanelAction extends JPanel {
 
 	private void initialize() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 0};
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0};
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
 
+		JLabel lblSamlMsgTitle = new JLabel("SAML Message");
+		GridBagConstraints gbc_lblSamlMsgTitle = new GridBagConstraints();
+		gbc_lblSamlMsgTitle.insets = new Insets(10, 5, 5, 0);
+		gbc_lblSamlMsgTitle.anchor = GridBagConstraints.WEST;
+		gbc_lblSamlMsgTitle.gridx = 0;
+		gbc_lblSamlMsgTitle.gridy = 0;
+		gbc_lblSamlMsgTitle.gridwidth = 3;
+		add(lblSamlMsgTitle, gbc_lblSamlMsgTitle);
+
+		btnSignatureReset = new JButton("Reset Message");
+		GridBagConstraints gbc_btnSignatureReset = new GridBagConstraints();
+		gbc_btnSignatureReset.anchor = GridBagConstraints.WEST;
+		gbc_btnSignatureReset.insets = new Insets(5, 5, 0, 0);
+		gbc_btnSignatureReset.gridx = 1;
+		gbc_btnSignatureReset.gridy = 1;
+		add(btnSignatureReset, gbc_btnSignatureReset);
+		btnSignatureReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller.resetMessage();
+			}
+		});
+
+		chkRawMode = new JCheckBox("Raw Mode (don't parse XML before sending)");
+		GridBagConstraints gbc_chkRawMode = new GridBagConstraints();
+		gbc_chkRawMode.anchor = GridBagConstraints.WEST;
+		gbc_chkRawMode.insets = new Insets(5, 5, 0, 0);
+		gbc_chkRawMode.gridx = 2;
+		gbc_chkRawMode.gridy = 1;
+		gbc_chkRawMode.gridwidth = 2;
+		add(chkRawMode, gbc_chkRawMode);
+		chkRawMode.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller.setRawMode(chkRawMode.isSelected());
+			}
+		});
+
 		JLabel lblXSWTitle = new JLabel("XSW Attacks");
 		GridBagConstraints gbc_lblXSWTitle = new GridBagConstraints();
-		gbc_lblXSWTitle.insets = new Insets(5, 5, 5, 0);
+		gbc_lblXSWTitle.insets = new Insets(10, 5, 5, 0);
 		gbc_lblXSWTitle.anchor = GridBagConstraints.WEST;
 		gbc_lblXSWTitle.gridx = 0;
-		gbc_lblXSWTitle.gridy = 0;
-		gbc_lblXSWTitle.gridwidth = 2;
+		gbc_lblXSWTitle.gridy = 3;
+		gbc_lblXSWTitle.gridwidth = 3;
 		add(lblXSWTitle, gbc_lblXSWTitle);
 
 		btnXSWHelp = new JButton("?");
 		GridBagConstraints gbc_btnXSWHelp = new GridBagConstraints();
 		gbc_btnXSWHelp.insets = new Insets(5, 5, 0, 0);
 		gbc_btnXSWHelp.gridx = 0;
-		gbc_btnXSWHelp.gridy = 1;
+		gbc_btnXSWHelp.gridy = 4;
 		btnXSWHelp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.showXSWHelp();
@@ -84,16 +114,16 @@ public class SamlPanelAction extends JPanel {
 		gbc_cmbboxXSW.insets = new Insets(5, 5, 0, 0);
 		gbc_cmbboxXSW.anchor = GridBagConstraints.WEST;
 		gbc_cmbboxXSW.gridx = 1;
-		gbc_cmbboxXSW.gridy = 1;
-		gbc_cmbboxXSW.gridwidth = 2;
+		gbc_cmbboxXSW.gridy = 4;
+		gbc_cmbboxXSW.gridwidth = 3;
 		add(cmbboxXSW, gbc_cmbboxXSW);
 
 		btnXSWPreview = new JButton("Preview in Browser...");
 		GridBagConstraints gbc_btnXSWPreview = new GridBagConstraints();
 		gbc_btnXSWPreview.anchor = GridBagConstraints.WEST;
 		gbc_btnXSWPreview.insets = new Insets(5, 5, 0, 0);
-		gbc_btnXSWPreview.gridx = 2;
-		gbc_btnXSWPreview.gridy = 2;
+		gbc_btnXSWPreview.gridx = 1;
+		gbc_btnXSWPreview.gridy = 5;
 		btnXSWPreview.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.showXSWPreview();
@@ -101,25 +131,12 @@ public class SamlPanelAction extends JPanel {
 		});
 		add(btnXSWPreview, gbc_btnXSWPreview);
 
-		btnSignatureReset = new JButton("Reset Message");
-		GridBagConstraints gbc_btnSignatureReset = new GridBagConstraints();
-		gbc_btnSignatureReset.anchor = GridBagConstraints.WEST;
-		gbc_btnSignatureReset.insets = new Insets(5, 5, 0, 0);
-		gbc_btnSignatureReset.gridx = 1;
-		gbc_btnSignatureReset.gridy = 2;
-		add(btnSignatureReset, gbc_btnSignatureReset);
-		btnSignatureReset.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controller.resetMessage();
-			}
-		});
-
 		btnXSWApply = new JButton("Apply XSW");
 		GridBagConstraints gbc_btnXSWApply = new GridBagConstraints();
 		gbc_btnXSWApply.insets = new Insets(5, 5, 0, 0);
 		gbc_btnXSWApply.anchor = GridBagConstraints.WEST;
-		gbc_btnXSWApply.gridx = 2;
-		gbc_btnXSWApply.gridy = 3;
+		gbc_btnXSWApply.gridx = 3;
+		gbc_btnXSWApply.gridy = 5;
 		btnXSWApply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.applyXSW();				
@@ -131,8 +148,8 @@ public class SamlPanelAction extends JPanel {
 		GridBagConstraints gbc_btnMatchAndReplace = new GridBagConstraints();
 		gbc_btnMatchAndReplace.insets = new Insets(5, 5, 0, 0);
 		gbc_btnMatchAndReplace.anchor = GridBagConstraints.WEST;
-		gbc_btnMatchAndReplace.gridx = 1;
-		gbc_btnMatchAndReplace.gridy = 3;
+		gbc_btnMatchAndReplace.gridx = 2;
+		gbc_btnMatchAndReplace.gridy = 5;
 		btnMatchAndReplace.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				showMatchAndReplaceDialog();
@@ -145,8 +162,8 @@ public class SamlPanelAction extends JPanel {
 		gbc_lblXmlAttacksTitle.anchor = GridBagConstraints.WEST;
 		gbc_lblXmlAttacksTitle.insets = new Insets(10, 5, 5, 0);
 		gbc_lblXmlAttacksTitle.gridx = 0;
-		gbc_lblXmlAttacksTitle.gridy = 4;
-		gbc_lblXmlAttacksTitle.gridwidth = 2;
+		gbc_lblXmlAttacksTitle.gridy = 7;
+		gbc_lblXmlAttacksTitle.gridwidth = 3;
 		add(lblXmlAttacksTitle, gbc_lblXmlAttacksTitle);
 
 		btnTestXXE = new JButton("Test XXE");
@@ -154,7 +171,7 @@ public class SamlPanelAction extends JPanel {
 		gbc_btnTestXXE.insets = new Insets(5, 5, 0, 0);
 		gbc_btnTestXXE.anchor = GridBagConstraints.WEST;
 		gbc_btnTestXXE.gridx = 1;
-		gbc_btnTestXXE.gridy = 5;
+		gbc_btnTestXXE.gridy = 8;
 		btnTestXXE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String collabUrl = JOptionPane.showInputDialog(btnXSWApply, 
@@ -171,7 +188,7 @@ public class SamlPanelAction extends JPanel {
 		gbc_btnTestXSLT.insets =new Insets(5, 5, 0, 0);
 		gbc_btnTestXSLT.anchor = GridBagConstraints.WEST;
 		gbc_btnTestXSLT.gridx = 2;
-		gbc_btnTestXSLT.gridy = 5;
+		gbc_btnTestXSLT.gridy = 8;
 		btnTestXSLT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String collabUrl = JOptionPane.showInputDialog(btnXSWApply, 
@@ -188,15 +205,15 @@ public class SamlPanelAction extends JPanel {
 		gbc_lblSignatureTitle.anchor = GridBagConstraints.WEST;
 		gbc_lblSignatureTitle.insets = new Insets(10, 5, 5, 0);
 		gbc_lblSignatureTitle.gridx = 0;
-		gbc_lblSignatureTitle.gridy = 7;
-		gbc_lblSignatureTitle.gridwidth = 2;
+		gbc_lblSignatureTitle.gridy = 10;
+		gbc_lblSignatureTitle.gridwidth = 3;
 		add(lblSignatureTitle, gbc_lblSignatureTitle);
 
 		btnSignatureHelp = new JButton("?");
 		GridBagConstraints gbc_btnSignatureHelp = new GridBagConstraints();
 		gbc_btnSignatureHelp.insets = new Insets(5, 5, 0, 0);
 		gbc_btnSignatureHelp.gridx = 0;
-		gbc_btnSignatureHelp.gridy = 8;
+		gbc_btnSignatureHelp.gridy = 11;
 		btnSignatureHelp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.showSignatureHelp();
@@ -209,8 +226,8 @@ public class SamlPanelAction extends JPanel {
 		gbc_cmbboxCertificate.insets = new Insets(5, 5, 0, 0);
 		gbc_cmbboxCertificate.anchor = GridBagConstraints.WEST;
 		gbc_cmbboxCertificate.gridx = 1;
-		gbc_cmbboxCertificate.gridy = 9;
-		gbc_cmbboxCertificate.gridwidth = 2;
+		gbc_cmbboxCertificate.gridy = 12;
+		gbc_cmbboxCertificate.gridwidth = 3;
 		add(cmbboxCertificate, gbc_cmbboxCertificate);
 
 		btnSignatureRemove = new JButton("Remove Signatures");
@@ -223,7 +240,7 @@ public class SamlPanelAction extends JPanel {
 		gbc_btnSignatureRemove.anchor = GridBagConstraints.WEST;
 		gbc_btnSignatureRemove.insets = new Insets(5, 5, 0, 0);
 		gbc_btnSignatureRemove.gridx = 1;
-		gbc_btnSignatureRemove.gridy = 8;
+		gbc_btnSignatureRemove.gridy = 11;
 		add(btnSignatureRemove, gbc_btnSignatureRemove);
 
 		btnSignatureReplace = new JButton("(Re-)Sign Assertion");
@@ -236,7 +253,7 @@ public class SamlPanelAction extends JPanel {
 		gbc_btnSignatureReplace.anchor = GridBagConstraints.WEST;
 		gbc_btnSignatureReplace.insets = new Insets(5, 5, 0, 0);
 		gbc_btnSignatureReplace.gridx = 1;
-		gbc_btnSignatureReplace.gridy = 10;
+		gbc_btnSignatureReplace.gridy = 13;
 		add(btnSignatureReplace, gbc_btnSignatureReplace);
 
 
@@ -245,7 +262,8 @@ public class SamlPanelAction extends JPanel {
 		gbc_btnSendCertificate.anchor = GridBagConstraints.WEST;
 		gbc_btnSendCertificate.insets = new Insets(5, 5, 0, 0);
 		gbc_btnSendCertificate.gridx = 2;
-		gbc_btnSendCertificate.gridy = 8;
+		gbc_btnSendCertificate.gridy = 11;
+		gbc_btnSendCertificate.gridwidth = 2;
 		btnSendCertificate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.sendToCertificatesTab();
@@ -263,7 +281,7 @@ public class SamlPanelAction extends JPanel {
 		});
 		gbc_btnSignatureAdd.insets = new Insets(5, 5, 0, 0);
 		gbc_btnSignatureAdd.gridx = 2;
-		gbc_btnSignatureAdd.gridy = 10;
+		gbc_btnSignatureAdd.gridy = 13;
 		add(btnSignatureAdd, gbc_btnSignatureAdd);
 
 		
@@ -272,10 +290,10 @@ public class SamlPanelAction extends JPanel {
 		lblMessage.setForeground(new Color(255, 140, 0));
 		GridBagConstraints gbc_lblMessage = new GridBagConstraints();
 		gbc_lblMessage.anchor = GridBagConstraints.WEST;
-		gbc_lblMessage.gridwidth = 2;
+		gbc_lblMessage.gridwidth = 3;
 		gbc_lblMessage.insets = new Insets(20, 5, 10, 0);
 		gbc_lblMessage.gridx = 0;
-		gbc_lblMessage.gridy = 11;
+		gbc_lblMessage.gridy = 14;
 		add(lblMessage, gbc_lblMessage);
 	}
 
@@ -308,7 +326,12 @@ public class SamlPanelAction extends JPanel {
 	public String getSearchText(){
 		return txtSearch.getText();
 	}
-	
+
+	public boolean isRawModeEnabled(){ return chkRawMode.isSelected(); }
+
+	public void setRawModeEnabled(boolean rawModeEnabled) {
+		chkRawMode.setSelected(rawModeEnabled);
+	}
 
 	public void disableControls() {
 		cmbboxCertificate.setEnabled(false);
@@ -325,6 +348,7 @@ public class SamlPanelAction extends JPanel {
 		btnMatchAndReplace.setEnabled(false);
 		btnTestXXE.setEnabled(false);
 		btnTestXSLT.setEnabled(false);
+		chkRawMode.setEnabled(false);
 		this.revalidate();
 	}
 
@@ -343,6 +367,7 @@ public class SamlPanelAction extends JPanel {
 		btnMatchAndReplace.setEnabled(true);
 		btnTestXXE.setEnabled(true);
 		btnTestXSLT.setEnabled(true);
+		chkRawMode.setEnabled(true);
 		this.revalidate();
 	}
 
